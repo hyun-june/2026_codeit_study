c71e057b101a2c9cc3b8c42e7b2ab64ac3a371a3
Hyunjun <leehyunjune11@naver.com>
2026-05-18 13:16:43 +0900

    update

--- 한계단/week1/app/routes/home.tsx
+++ 한계단/week1/app/routes/home.tsx
@@ -18,6 +18,7 @@ export default function Home() {
     if (input.trim().length === 0) return;
     const newId = Math.max(...tasks.map((item) => Number(item.id))) + 1;
     setTasks((prev) => [...prev, { id: newId, task: input }]);
+    setInput("");
   };
 
   const deleteTask = (id: number) => {
@@ -33,6 +34,7 @@ export default function Home() {
           type="text"
           placeholder="Add your task"
           onChange={handleChange}
+          value={input}
         />
         <div>
           <button onClick={addTask}>Submit</button>
